542bb047d7b9abacfaac996c6ec50abfd104efd3
AttilaWiedemann <attila.wiedemann@gmail.com>
2020-05-11 00:55:40 +0200

    adds webSecConfig

---
+++ src/main/java/backend/config/WebSecConfig.java
@@ -0,0 +1,51 @@
+package backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+public class WebSecConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/main", true)//TODO
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login", "/css/**", "/spaceElements/**", "/icons/**", "/cheeses/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
